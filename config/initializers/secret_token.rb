883dc2db990373c0f067259f5a9063fe7611458f
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2011-03-15 16:13:43 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookBuy::Application.config.secret_token = '743c60d67ff2213aa74d5ab7057b45dfc6d1ab6143033010bb8592296918b4d4462ba89c860b65511e3100559a621afc4230e1c78795ddfb5192c79d1834af27'
