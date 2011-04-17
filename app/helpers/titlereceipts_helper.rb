2762ec0293792f23e22be00ce4fbe6b5962b16d0
akhilesh <kataria.subs@gmail.com>
2011-04-17 16:47:46 +0530

    fixed bug: was returing object instead of title attribute

--- app/helpers/titlereceipts_helper.rb
+++ app/helpers/titlereceipts_helper.rb
@@ -1,9 +1,9 @@
 module TitlereceiptsHelper
   def title_for_isbn(isbn)
     if isbn.length == 13 then
-      Enrichedtitle.find_by_isbn(isbn)
+      Enrichedtitle.find_by_isbn(isbn).title
     elsif isbn.length == 10 then
-      Enrichedtitle.find_by_isbn10(isbn)
+      Enrichedtitle.find_by_isbn10(isbn).title
     else
       "No Title Found"
     end
