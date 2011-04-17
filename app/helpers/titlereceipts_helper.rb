2e58d9c77ea56babeb8839e2c8b55d77ef353c18
akhilesh <kataria.subs@gmail.com>
2011-04-17 16:41:51 +0530

    returning title for titlereceipt add

--- app/helpers/titlereceipts_helper.rb
+++ app/helpers/titlereceipts_helper.rb
@@ -1,2 +1,11 @@
 module TitlereceiptsHelper
+  def title_for_isbn(isbn)
+    if isbn.length == 13 then
+      Enrichedtitle.find_by_isbn(isbn)
+    elsif isbn.length == 10 then
+      Enrichedtitle.find_by_isbn10(isbn)
+    else
+      "No Title Found"
+    end
+  end
 end
