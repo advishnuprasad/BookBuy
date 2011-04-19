2ce4aa8496b7c2bd55a5e1ed7a3155f173f2e28d
Subhash Bhushan <subhash.bhushan@stratalabs.in>
2011-04-19 12:44:14 +0530

    Crating by Suppliers

--- app/models/box.rb
+++ app/models/box.rb
@@ -16,7 +16,10 @@
 class Box < ActiveRecord::Base
   belongs_to :crate
   
-  scope :unassigned, where("crate_id IS NULL")  
+  scope :unassigned, where("crate_id IS NULL").order("id")
+  scope :unassigned_among_pos, lambda { |po_nos|
+    where(:po_no => po_nos).order("id")
+    }
   scope :unassigned_in_po_and_invoice, lambda { |po_no, invoice_no|
     where("crate_id IS NULL AND po_no = ? and invoice_no = ?", po_no, invoice_no)
     }  
@@ -24,15 +27,32 @@ class Box < ActiveRecord::Base
     where("crate_id = :crate_id", :crate_id => crate_id)
     }
   
+  def get_supplier_name
+    Po.find_by_code(po_no).supplier.name
+  end
+  
   def self.fill_crate(crate_id)
     current_cnt = 0
     added_boxes = Array.new
     
-    #Find total count of books in un-assigned boxes by PO-Invoice
-    Box.unassigned.each do |box|
-      if current_cnt + box.total_cnt < Crate::CAPACITY
-        current_cnt = current_cnt + box.total_cnt
-        added_boxes.push(box)
+    unassigned_boxes = Box.unassigned.unassigned_among_pos(Po.pos_for_supplier(Po.find_by_code(Box.unassigned.first.po_no).supplier_id).collect {|po| po.code})
+    
+    #More Intelligence
+    #1. Get the Biggest boxes first
+    #2. The first box might be lesser than capacity, but the next may be more than capacity
+    
+    #If first crate has more books than capacity
+    if unassigned_boxes.first.total_cnt > Crate::CAPACITY
+      box = unassigned_boxes.first
+      current_cnt = current_cnt + box.total_cnt
+      added_boxes.push(box)
+    #else loop and fill crate
+    else
+      unassigned_boxes.each do |box|
+        if current_cnt + box.total_cnt < Crate::CAPACITY
+          current_cnt = current_cnt + box.total_cnt
+          added_boxes.push(box)
+        end
       end
     end
     
